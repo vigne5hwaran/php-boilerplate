c40459963fb4abe1158cd20865df6c2ecd3703fa
vignesh <vw170996@gmail.com>
2019-07-08 15:58:06 +0530

    just a change

--- pages/404.php
+++ pages/404.php
@@ -9,7 +9,6 @@ $uri = end($uri);
 <div class="error-page module text-center">
 
     <div class="grid-container grid-x grid-padding-x">
-        <h2></h2>
         <h2>404! Page is not found!</h2>
         <p>The page "<?php echo $uri; ?>" is not found,We are sorry but there is nothing matched to your criteria. Please use Different keyword.
         </p>
