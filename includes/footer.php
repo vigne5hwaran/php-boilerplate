6a5208aa9606f8087df031a76de06a91974698f3
vignesh <vw170996@gmail.com>
2020-02-17 15:48:09 +0530

    minified

--- includes/footer.php
+++ includes/footer.php
@@ -1,135 +1,13 @@
 	<!-- Start Footer 
     ============================================= -->
     <footer class="bg-dark text-light">
-        <div class="container">
-            <div class="row">
-                <div class="f-items default-padding">
-
-                    <!-- Single Item -->
-                    <div class="col-md-3 col-sm-6 equal-height item">
-                        <div class="f-item">
-                            <h4>About</h4>
-                            <p>
-                                Excellence decisively nay man yet impression for contrasted remarkably. There spoke
-                                happy for you are out. Fertile how old address.
-                            </p>
-                            <div class="opening-info">
-                                <h5>Opening Hours</h5>
-                                <ul>
-                                    <li> <span> Mon - Tues : </span>
-                                        <div class="pull-right"> 6.00 am - 10.00 pm </div>
-                                    </li>
-                                    <li> <span> Wednes - Thurs :</span>
-                                        <div class="pull-right"> 8.00 am - 6.00 pm </div>
-                                    </li>
-                                    <li> <span> Sun : </span>
-                                        <div class="pull-right closed"> Closed </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Item -->
-                    <!-- Single Item -->
-                    <div class="col-md-3 col-sm-6 equal-height item">
-                        <div class="f-item link">
-                            <h4>Treatment</h4>
-                            <ul>
-                                <li>
-                                    <a href="gi-oncology"><i class="fas fa-arrow-right"></i> Gi Oncology</a>
-                                </li>
-                                <li>
-                                    <a href="urology"><i class="fas fa-arrow-right"></i> Urology</a>
-                                </li>
-                                <li>
-                                    <a href="breast"><i class="fas fa-arrow-right"></i> Breast</a>
-                                </li>
-                                <li>
-                                    <a href="gynaec-oncology"><i class="fas fa-arrow-right"></i> Gynaec oncology</a>
-                                </li>
-                                <li>
-                                    <a href="bone"><i class="fas fa-arrow-right"></i> Bone</a>
-                                </li>
-                                <li>
-                                    <a href="skin"><i class="fas fa-arrow-right"></i> Skin</a>
-                                </li>
-                                <li>
-                                    <a href="thyroid"><i class="fas fa-arrow-right"></i> Thyroid</a>
-                                </li>
-                                <li>
-                                    <a href="hipec-surgery"><i class="fas fa-arrow-right"></i> HIPEC Surgery</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- End Single Item -->
-                    <!-- Single Item -->
-                    <div class="col-md-3 col-sm-6 equal-height item">
-                        <div class="f-item twitter-widget">
-                            <h4>Latest tweets</h4>
-                            <div class="twitter-item">
-                                <div class="twitter-content">
-                                    <p>
-                                        <a href="#">@Becare</a> Looking for an awesome CREATIVE WordPress Theme? Find it
-                                        here: <a target="_blank"
-                                            href="http://t.co/0WWEMQEQ48">http://t.co/0WWEMQEQ48</a>
-                                    </p>
-                                </div>
-                                <div class="twitter-context">
-                                    <i class="fab fa-twitter"></i><span class="twitter-date"> 01 day ago</span>
-                                </div>
-                            </div>
-                            <div class="twitter-item">
-                                <div class="twitter-content">
-                                    <p>
-                                        <a href="#">@Jisham</a> It is a long established fact that a reader will be
-                                        distracted by the readable . Find it here: <a target="_blank"
-                                            href="http://t.co/0WWEMQEQ48">http://t.co/0WWEMQEQ48</a>
-                                    </p>
-                                </div>
-                                <div class="twitter-context">
-                                    <i class="fab fa-twitter"></i><span class="twitter-date"> 02 days ago</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Single Item -->
-                    <!-- Single Item -->
-                    <div class="col-md-3 col-sm-6 equal-height item">
-                        <div class="f-item contact">
-                            <h4>Contact</h4>
-                            <ul>
-                                <li>
-                                    <i class="fas fa-phone"></i>
-                                    <p>Phone <span>+044 - 4295 6777</span></p>
-                                </li>
-                                <li>
-                                    <i class="fas fa-envelope"></i>
-                                    <p>Email <span>
-                                        <a href="mailto:ayyappas1@yahoo.co.uk">ayyappas1@yahoo.co.uk</a>
-                                    </span>
-                                    </p>
-                                </li>
-                                <li>
-                                    <i class="fas fa-map"></i>
-                                    <p>Location <span>Kumaran Hospital, No. 214, E.V.R Periyar Salai, 
-                                Poonamallee High Road, Kilpauk, Chennai, Tamil Nadu 600010 India</span></p>
-                                </li>
-                            </ul>
-                            
-                        </div>
-                    </div>
-                    <!-- End Single Item -->
-                </div>
-            </div>
-        </div>
         <!-- Start Footer Bottom -->
         <div class="footer-bottom bg-dark text-light">
             <div class="container">
                 <div class="row">
                     <div class="col-md-6">
                         <p>&copy; Copyright 2019. All Rights Reserved. Designed by 
-                            <a href="javascript:void();">Brite Infomedia</a>
+                            <a href="javascript:void();">My company</a>
                         </p>
                     </div>
                     <div class="col-md-6 text-right link">
